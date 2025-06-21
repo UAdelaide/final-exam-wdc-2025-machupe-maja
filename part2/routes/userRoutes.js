e9a1c2a595909203ed512e5757c6847b4ad6297b
Machu <machu@machupicchu.com>
2025-06-21 04:28:38 +0000

    [automatic commit] 2025-06-21 04:28:37 UTC ;; communications error to 208.67.222.222#53: timed out 1.147.68.158

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
     }
 
     // Store the login in the session
-    req.session.user = rows[0]
+    req.session.user = rows[0];
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });

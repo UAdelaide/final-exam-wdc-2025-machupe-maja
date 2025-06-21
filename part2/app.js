fac57c782d22d13c954d8414544b113384e4fac7
Machu <machu@machupicchu.com>
2025-06-21 06:22:04 +0000

    [automatic commit] 2025-06-21 06:22:04 UTC 1.147.68.158

--- part2/app.js
+++ part2/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-
+const db = require('./models/db');
 const app = express();
 
 // Middleware

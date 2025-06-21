d9587bcc8b554eb95e407811c88135aa243b0b3e
Machu <machu@machupicchu.com>
2025-06-21 06:12:54 +0000

    [automatic commit] 2025-06-21 06:12:54 UTC 1.147.68.158

--- part2/app.js
+++ part2/app.js
@@ -25,5 +25,32 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// GET all dogs
+
+
+router.get('/api/dogs', async (req, res, next) => {
+  const dogs_sql = `
+    SELECT
+      name AS dog_name,
+      size,
+      username AS owner_username
+    FROM
+      Dogs d
+    INNER JOIN
+      Users u
+      ON u.user_id = d.owner_id`;
+  try {
+    const [rows] = await req.db.query(dogs_sql);
+    // Returning as pure JSON instead of stringified
+    res.json(rows);
+  } catch (err) {
+    // 500 Since it's likely a server related problem
+    res.status(500);
+    // Noone let the dogs out :(
+    res.status(500).json({ error: 'Error trying to let the dogs out!' });
+    console.error('Error trying to let the dogs out! \n', err);
+  }
+});
+
 // Export the app instead of listening here
 module.exports = app;

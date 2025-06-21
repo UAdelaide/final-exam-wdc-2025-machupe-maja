51e06c742bd49c29ddf626948fac1d833ee627d8
Machu <machu@machupicchu.com>
2025-06-21 04:08:25 +0000

    [automatic commit] 2025-06-21 04:08:25 UTC 1.147.68.158

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,12 +37,12 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
+      WHERE username = ? AND password_hash = ?
     `, [email, password]);
 
     if (rows.length === 0) {
